e58a5a528ccdc7e98d6ea4d85bfafd0705805477
tks10 <tktktks10@gmail.com>
2018-11-09 20:31:41 +0900

    feat: exhaustive search

--- tsp.py
+++ tsp.py
@@ -1,10 +1,36 @@
 import numpy as np
+import itertools
 
 
 class TspSolver:
     def __init__(self, adjacent):
         self._adjacent = np.asarray(adjacent)
 
+    def exhaustive_search(self):
+        origin = range(len(self._adjacent))
+        paths = list(itertools.permutations(origin))
+        min_ = np.inf
+        best_path = None
+
+        for path in paths:
+            cost = self.get_cost(path)
+            if cost < min_:
+                min_ = cost
+                best_path = list(path) + [path[0]]
+                print("[Cost]", min_)
+                print("[Path]", " > ".join(map(str, best_path)), "\n")
+
+        return min_, best_path
+
+    def get_cost(self, path):
+        sum_ = 0
+        for i in range(len(path)-1):
+            sum_ += self._adjacent[path[i]][path[i+1]]
+        sum_ += self._adjacent[path[-1]][0]
+
+        return sum_
+
+    # 分枝わからん
     def solve(self, start, current_path=[]):
         if start in current_path:
             return
@@ -27,6 +53,8 @@ class TspSolver:
         print(lower_use, lower_unuse)
 
         if lower_use <= lower_unuse:
+            self._adjacent[start][dst] = 0
+            print(self._adjacent)
             self.solve(dst, current_path)
         else:
             self._adjacent = adjacent_unuse
@@ -35,7 +63,7 @@ class TspSolver:
 
 
     @staticmethod
-    def calc_lower_bound(src, paths):
+    def calc_lower_bound(src):
         row_mins = src.min(axis=1)
         result = src - np.reshape(row_mins, (-1, 1))
         col_mins = result.min(axis=0)
@@ -70,7 +98,7 @@ def main():
     ]
 
     solver = TspSolver(adjacent2)
-    solver.solve(start=2)
+    solver.exhaustive_search()
 
 
 if __name__ == '__main__':
