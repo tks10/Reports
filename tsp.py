d137706194df0de7c370c1dc64974c091076fa1f
tks10 <tktktks10@gmail.com>
2018-11-12 16:53:25 +0900

    done

--- tsp.py
+++ tsp.py
@@ -1,5 +1,6 @@
 import numpy as np
 import itertools
+np.seterr(divide='ignore', invalid='ignore')
 
 
 class TspSolver:
@@ -16,9 +17,9 @@ class TspSolver:
             cost = self.get_cost(path)
             if cost < min_:
                 min_ = cost
-                best_path = list(path) + [path[0]]
+                best_path = list(path)
                 print("[Cost]", min_)
-                print("[Path]", " > ".join(map(str, best_path)), "\n")
+                print("[Path]", " > ".join(map(lambda p: str(p+1), best_path)), "\n")
 
         return min_, best_path
 
@@ -30,36 +31,45 @@ class TspSolver:
 
         return sum_
 
-    # 分枝わからん
-    def solve(self, start, current_path=[]):
-        if start in current_path:
-            return
+    def branch_and_bound_search(self, start, current_map=None, current_path=[]):
         current_path.append(start)
-        print()
-        print(current_path)
+        if len(current_path) == len(self._adjacent):
+            return self.get_cost(current_path), current_path
+
+        # Init
+        if current_map is None:
+            current_map = self._adjacent
 
         # Get the potential destination
-        dst = np.argmin(self._adjacent[start])
+        dst = np.argmin(current_map[start])
 
         # Check in case of use and unuse
-        adjacent = np.copy(self._adjacent)
-        adjacent_unuse = np.copy(self._adjacent)
+        adjacent = np.copy(current_map)
+        adjacent_unuse = np.copy(current_map)
+
+        # Update(Use)
+        tmp = adjacent[start][dst]
+        adjacent[start, :] = np.inf
+        adjacent[:, dst] = np.inf
+        adjacent[start][dst] = tmp
+
+        # Update(Unuse)
         adjacent_unuse[start][dst] = np.inf
 
         # Get lowers
         lower_use = self.calc_lower_bound(adjacent)
         lower_unuse = self.calc_lower_bound(adjacent_unuse)
-        print(start+1, dst+1)
-        print(lower_use, lower_unuse)
+
+        print("[Determined]", " > ".join(map(str, current_path)))
+        print("[Evaluating]", start+1, ">", dst+1)
+        print("[Lower bounds(include, exclude)]", lower_use, lower_unuse, "\n")
 
         if lower_use <= lower_unuse:
-            self._adjacent[start][dst] = 0
-            print(self._adjacent)
-            self.solve(dst, current_path)
+            return self.branch_and_bound_search(dst, adjacent, current_path)
+
         else:
-            self._adjacent = adjacent_unuse
             current_path.remove(current_path[-1])
-            self.solve(start, current_path)
+            return self.branch_and_bound_search(start, adjacent_unuse, current_path)
 
 
     @staticmethod
@@ -67,8 +77,10 @@ class TspSolver:
         row_mins = src.min(axis=1)
         result = src - np.reshape(row_mins, (-1, 1))
         col_mins = result.min(axis=0)
-        result = result - col_mins
         lower = row_mins.sum() + col_mins.sum()
+
+        if np.isnan(result).any():
+            return np.inf
 
         return lower
 
@@ -81,7 +93,7 @@ class TspSolver:
 
 
 def main():
-    adjacent = [
+    adjacent_report = [
         [np.inf, 21, 5, 15, 9],
         [17, np.inf, 12, 6, 24],
         [13, 5, np.inf, 20, 8],
@@ -89,7 +101,7 @@ def main():
         [26, 7, 13, 8, np.inf]
     ]
 
-    adjacent2 = [
+    adjacent_sample = [
         [np.inf, 21, 7, 13, 15],
         [11, np.inf, 19, 12, 25],
         [15, 24, np.inf, 13, 5],
@@ -97,8 +109,19 @@ def main():
         [28, 6, 11, 5, np.inf]
     ]
 
-    solver = TspSolver(adjacent2)
-    solver.exhaustive_search()
+    solver = TspSolver(adjacent_report)
+
+    cost, path = solver.exhaustive_search()
+    print("************* Result(Exhaustive) *************")
+    print("[Path]", " > ".join(map(lambda p: str(p + 1), path + [path[0]])))
+    print("[Cost]", cost)
+    print("**********************************************\n\n")
+
+    cost, path = solver.branch_and_bound_search(start=2)
+    print("*********** Result(Branch and Bound) ***********")
+    print("[Path]", " > ".join(map(lambda p: str(p+1), path + [path[0]])))
+    print("[Cost]", cost)
+    print("************************************************\n\n")
 
 
 if __name__ == '__main__':
