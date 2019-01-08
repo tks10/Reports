54f10e0415a08854e9a48b2b3f9776b180ebf979
tks10 <tktktks10@gmail.com>
2019-01-08 15:18:00 +0900

    feat: define methods

--- hop.py
+++ hop.py
@@ -1,4 +1,15 @@
 
+class HopFieldNetwork:
+    def __init__(self, row=3, col=3):
+        self._row, self._col = row, col
+
+    def memorize(self, states):
+        pass
+
+    def recall(self, initial_state):
+        pass
+
+
 def main():
     pass
 
