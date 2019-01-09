5388eb254e5ee9e66157a29c001d50a84f9cd83b
tks10 <tktktks10@gmail.com>
2019-01-09 17:04:02 +0900

    feat: visualize

--- hop.py
+++ hop.py
@@ -1,3 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+
+
 class HopFieldNetwork:
     def __init__(self, size=9):
         self._size = size
@@ -36,9 +41,32 @@ class HopFieldNetwork:
             for j in range(self._size):
                 self._weight_adjacent[i][j] = 0
 
+    def show_nodes(self):
+        nodes_np = np.asarray(self._nodes)
+        size = int(math.sqrt(self._size))
+        HopFieldNetwork.show_matrix(nodes_np.reshape((size, size)))
+
+    @staticmethod
+    def show_matrix(data):
+        data = np.asarray(data)
+        fig, ax = plt.subplots()
+        ax.pcolor(data, cmap=plt.cm.Blues)
+
+        ax.set_xticks(np.arange(data.shape[0]) + 0.5, minor=False)
+        ax.set_yticks(np.arange(data.shape[1]) + 0.5, minor=False)
+
+        ax.invert_yaxis()
+        ax.xaxis.tick_top()
+
+        plt.show()
+
 
 def main():
-    pass
+    state = [[1, 0, 1, 0, 1, 0, 1, 0, 1]]
+
+    hfn = HopFieldNetwork()
+    hfn.recall(state[0])
+    hfn.show_nodes()
 
 
 if __name__ == "__main__":
