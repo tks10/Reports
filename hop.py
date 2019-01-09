5cd348a0cc3ff04d849468fdc00f190ce57f4cde
tks10 <tktktks10@gmail.com>
2019-01-09 16:37:02 +0900

    update: logic

--- hop.py
+++ hop.py
@@ -1,10 +1,17 @@
-
 class HopFieldNetwork:
-    def __init__(self, row=3, col=3):
-        self._row, self._col = row, col
+    def __init__(self, size=9):
+        self._size = size
+        self._nodes = [0 for _ in range(self._size)]
+        self._weight_adjacent = list()
+        for _ in range(self._size):
+            self._weight_adjacent.append([0 for _ in range(self._size)])
 
     def memorize(self, states):
-        pass
+        for state in states:
+            if self._size != len(state):
+                raise ValueError
+
+        
 
     def recall(self, initial_state):
         pass
