e6b3a53e35d9a5e18862d2f20daf803b9108a637
tks10 <tktktks10@gmail.com>
2019-01-09 16:44:42 +0900

    feat: memorize

--- hop.py
+++ hop.py
@@ -11,10 +11,23 @@ class HopFieldNetwork:
             if self._size != len(state):
                 raise ValueError
 
-        
+        self.init_weights()
+
+        for state in states:
+            for i in range(self._size):
+                alpha_i = state[i]
+                for j in range(self._size):
+                    alpha_j = state[j]
+                    if i != j:
+                        self._weight_adjacent[i][j] += (2 * alpha_i - 1) * (2 * alpha_j - 1)
 
     def recall(self, initial_state):
         pass
+
+    def init_weights(self):
+        for i in range(self._size):
+            for j in range(self._size):
+                self._weight_adjacent[i][j] = 0
 
 
 def main():
